45f90141816e7fba7f493c79c8207f7ac68f9559
ubiibuuu <ubiibuuu@gmail.com>
2024-04-25 18:59:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eveniet architecto quos repellendus ipsam deserunt maxime ipsum rerum qui eos? Recusandae, exercitationem nesciunt! Unde vitae suscipit nisi aspernatur hic recusandae!</p>
+    <li>satu</li>
+    <li>dua</li>
     
 </body>
 </html>
